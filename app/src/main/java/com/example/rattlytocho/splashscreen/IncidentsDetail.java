0ac1803916ae392f9c6889ada5b8b3ce39fc7f29
rattlytocho <rattlytocho@gmail.com>
2017-03-29 14:07:35 +0000

    butter knife

---
+++ app/src/main/java/com/example/rattlytocho/splashscreen/IncidentsDetail.java
@@ -0,0 +1,18 @@
+package com.example.rattlytocho.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by rattlytocho on 29/03/17.
+ */
+
+public class IncidentsDetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incidents_detail);
+    }
+}
